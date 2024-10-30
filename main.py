fe72d4d8cc0c44abab8b585c491833bb4b0a0914
DuzhonkovaMarina <merelin.lin@gmail.com>
2024-10-31 00:34:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+Number_of_completed_homework_assignments = 12
+Number_of_hours_spent = 1.5
+Cours_name = 'Python'
+Time_for_one_task = (1.5/12)
+print('Курс: ', Cours_name, ';', 'Всего задач: ', Number_of_completed_homework_assignments, ';', 'Затрачено часов: ', Number_of_hours_spent, ';', 'Среднее время выполнения: ', Time_for_one_task)
